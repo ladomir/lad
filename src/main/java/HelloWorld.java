580a864372369a8e976865dd61aab67c714388de
ladomir <i.zhuravlev2603@gmail.com>
2020-09-05 13:53:39 +0300

    Commit 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String string = new String();
+        string = "Hello World!";
+        System.out.println(string);
     }
 }
